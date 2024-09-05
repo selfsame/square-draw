46f560809098128563ab7651cbcfa3cda2eab4fa
selfsame <joseph.parker.1@gmail.com>
2024-09-05 16:16:19 -0400

    movable elements, delete element, fillcolor input

--- src/square_draw/core.cljs
+++ src/square_draw/core.cljs
@@ -5,11 +5,9 @@
 
 (defonce state (r/atom {
   :tool :draw
-  :file [
-    {:x 20 :y 20 :w 100 :h 60 :fill "#ff3300"}
-    ]
-  :selected 0
-  }))
+  :color "#bada55"
+  :file []
+  :selected 0}))
 
 
 
@@ -25,9 +23,7 @@
         (.fillRect ctx (:x el) (:y el) (:w el) (:h el)))))
   (when-let [el (get (:file @state) (:selected @state))]
     (.setLineDash ctx #js [6])
-    (.strokeRect ctx (:x el) (:y el) (:w el) (:h el))
-  )
-  )
+    (.strokeRect ctx (:x el) (:y el) (:w el) (:h el))))
 
 ; ideally this would schedule render! in a requestAnimationFrame
 (add-watch state :render
@@ -49,14 +45,14 @@
       (fn [[idx {:keys [x y w h]}]]
         (and (< x cx (+ x w))
              (< y cy (+ y h))))
-      (map-indexed vector (:file @state)))))
+      (reverse (map-indexed vector (:file @state))))))
 
 (defn on-draw [e]
   ; note the initial mouse x/y and create a new element in the file vector
   ; then set up a mouse move fn that modifies this new element's width based on the current mouse position
   (let [[x y] (mouse->canvas (.-x e) (.-y e))
         idx (count (:file @state))]
-    (swap! state update :file conj {:x x :y y :w 0 :h 0 :fill "orange"})
+    (swap! state update :file conj {:x x :y y :w 0 :h 0 :fill (:color @state)})
     (swap! state assoc :selected idx)
     (swap! state assoc :movefn 
       (fn [e]
@@ -67,18 +63,26 @@
     ))
 
 (defn on-move [e]
-  (if-let [[idx _] (first (pick (.-x e) (.-y e)))]
-    (do (swap! state assoc :selected idx)
-      )
-    (swap! state assoc :selected nil)
-    ))
+  (let [[x y] [(.-x e) (.-y e)]]
+    (if-let [[idx picked] (first (pick (.-x e) (.-y e)))]
+      (do 
+        (swap! state assoc :selected idx)
+        (swap! state assoc :movefn 
+          ; calculate the delta mouse x/y from the start of the move then add that to the inital
+          ; picked element's position
+          (fn [e]
+            (let [[nx ny] [(.-x e) (.-y e)]]
+              (swap! state update-in [:file idx] assoc 
+                :x (+ (:x picked) (- nx x)) 
+                :y (+ (:y picked) (- ny y)))))))
+      (swap! state assoc :selected nil)
+      )))
 
 ; Input cycles, dispatch on mouse down based on the current tool.  If move/up handlers are needed
 ; they can be set up in the dispatched fn
 
 (defn pointer-down [e]
-  (js/console.log e)
-  (prn (pick (.-x e) (.-y e)))
+  ;(js/console.log e)
   (when (= (.-target e) canvas)
     (cond
       (= (:tool @state) :draw) (on-draw e)
@@ -89,7 +93,7 @@
   (if (:movefn @state) ((:movefn @state) e)))
 
 (defn pointer-up [e]
-  (js/console.log e)
+  ;(js/console.log e)
   (if (:upfn @state) ((:upfn @state) e))
   (swap! state dissoc :movefn :upfn))
 
@@ -111,7 +115,21 @@
               (swap! state assoc :tool k)
               (js/document.body.setAttribute "class" (name k)))}
             k])
-        [:move :draw]))])
+        [:move :draw]))
+    [:div "fillcolor"]
+    [:input {
+      :type :color 
+      :value (:color @state)
+      :on-change (fn [e] (swap! state assoc :color (.. e -target -value)))}]
+    (when-let [idx (:selected @state)]
+      [:button {
+        :on-click (fn [e] 
+          (swap! state assoc 
+            :file (vec (concat 
+                    (subvec (:file @state) 0 idx)
+                    (subvec (:file @state) (inc idx))))
+            :selected nil))} 
+       "delete"])])
 
 (defn root-ui []
   [:div
